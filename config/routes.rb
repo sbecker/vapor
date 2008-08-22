894720202b5db22058c7331d5f1655e6b9106fa5
Scott <scottbecker@joydivider-mini.local>
2008-08-21 22:17:42 -0700

    add user member resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
+  # Restful Authentication Named Routes
   map.logout   '/logout',                    :controller => 'sessions', :action => 'destroy'
   map.login    '/login',                     :controller => 'sessions', :action => 'new'
   map.register '/register',                  :controller => 'users',    :action => 'create'
   map.signup   '/signup',                    :controller => 'users',    :action => 'new'
   map.activate '/activate/:activation_code', :controller => 'users',    :action => 'activate', :activation_code => nil
-
-  map.resources :users
+  
+  # Restful Authentication Resource Routes
+  map.resources :users, :member => { :suspend   => :put,
+                                     :unsuspend => :put,
+                                     :purge     => :delete }
   map.resource :session
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
