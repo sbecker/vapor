4f34506cc3491462210a9eaa304743713653133c
Scott Becker <becker.scott@gmail.com>
2008-08-21 23:09:10 -0700

    minor layout adjustments

--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -7,6 +7,6 @@ describe "/dashboard/index" do
   
   #Delete this example and add some real ones or delete this file
   it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/dashboard/index])
+    response.should have_tag('h2', %r[Dashboard])
   end
 end
