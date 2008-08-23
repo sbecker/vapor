f212b70b5674a696c541bdef15bc28c183e4378d
Scott Becker <becker.scott@gmail.com>
2008-08-23 01:09:46 -0700

    truncate the super secret account keys when displaying in views

--- spec/views/accounts/edit.html.erb_spec.rb
+++ spec/views/accounts/edit.html.erb_spec.rb
@@ -10,8 +10,8 @@ describe "/accounts/edit.html.erb" do
       :aws_account_number => "value for aws_account_number",
       :aws_access_key => "value for aws_access_key",
       :aws_secret_access_key => "value for aws_secret_access_key",
-      :aws_x_509_key => "value for aws_x_509_key",
-      :aws_x_509_certificate => "value for aws_x_509_certificate"
+      :aws_x_509_key => "-----BEGIN PRIVATE KEY----- asdf87234kjhsdfjkhasdfkjh34hkjh23kjh34kjh34",
+      :aws_x_509_certificate => "-----BEGIN CERTIFICATE----- alskdjfhasdjklfhasdlkjfhasdflkjhasldjflasdjkf"
     )
   end
 
@@ -22,9 +22,9 @@ describe "/accounts/edit.html.erb" do
       with_tag('input#account_name[name=?]', "account[name]")
       with_tag('input#account_aws_account_number[name=?]', "account[aws_account_number]")
       with_tag('input#account_aws_access_key[name=?]', "account[aws_access_key]")
-      with_tag('input#account_aws_secret_access_key[name=?]', "account[aws_secret_access_key]")
-      with_tag('textarea#account_aws_x_509_key[name=?]', "account[aws_x_509_key]")
-      with_tag('textarea#account_aws_x_509_certificate[name=?]', "account[aws_x_509_certificate]")
+      with_tag('input#account_aws_secret_access_key[name=?][value=?]', "account[aws_secret_access_key]", template.truncate(@account.aws_secret_access_key, 20))
+      with_tag('textarea#account_aws_x_509_key[name=?]', "account[aws_x_509_key]", template.truncate(@account.aws_x_509_key, 60))
+      with_tag('textarea#account_aws_x_509_certificate[name=?]', "account[aws_x_509_certificate]", template.truncate(@account.aws_x_509_certificate, 60))
     end
   end
 end
--- spec/views/accounts/show.html.erb_spec.rb
+++ spec/views/accounts/show.html.erb_spec.rb
@@ -9,19 +9,21 @@ describe "/accounts/show.html.erb" do
       :aws_account_number => "value for aws_account_number",
       :aws_access_key => "value for aws_access_key",
       :aws_secret_access_key => "value for aws_secret_access_key",
-      :aws_x_509_key => "value for aws_x_509_key",
-      :aws_x_509_certificate => "value for aws_x_509_certificate"
+      :aws_x_509_key => "-----BEGIN PRIVATE KEY----- asdf87234kjhsdfjkhasdfkjh34hkjh23kjh34kjh34",
+      :aws_x_509_certificate => "-----BEGIN CERTIFICATE----- alskdjfhasdjklfhasdlkjfhasdflkjhasldjflasdjkf"
     )
   end
 
   it "should render attributes in <p>" do
     render "/accounts/show.html.erb"
-    response.should have_text(/value\ for\ name/)
-    response.should have_text(/value\ for\ aws_account_number/)
-    response.should have_text(/value\ for\ aws_access_key/)
-    response.should have_text(/value\ for\ aws_secret_access_key/)
-    response.should have_text(/value\ for\ aws_x_509_key/)
-    response.should have_text(/value\ for\ aws_x_509_certificate/)
+    response.should have_tag('h2', /Account/)
+
+    response.should have_text(%r[#{@account.name}])
+    response.should have_text(%r[#{@account.aws_account_number}])
+    response.should have_text(%r[#{@account.aws_access_key}])
+    response.should have_text(%r[#{template.truncate(@account.aws_secret_access_key, 20)}])
+    response.should have_text(%r[#{template.truncate(@account.aws_x_509_key, 60)}])
+    response.should have_text(%r[#{template.truncate(@account.aws_x_509_certificate, 60)}])
   end
 end
 
